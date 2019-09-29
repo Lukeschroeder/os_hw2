7f20de32e7348cd569df45ac49be5d72929aef66
entertheanorak <anstmfriends@gmail.com>
2019-09-28 20:01:33 -0400

    updates with what Badri Nath told me

--- my_pthread.c
+++ my_pthread.c
@@ -2,29 +2,49 @@
 
 /* Scheduler State */
  // Fill in Here //
-typedef enum schedularState{
-  ON,//running
-  OFF//not running
-}state;
-state sched=OFF;
-
+struct threadControlBlock* r_tail=NULL;
+struct threadControlBlock* s_tail=NULL;
+struct threadControlBlock* execute=NULL;
 
 /* Scheduler Function
  * Pick the next runnable thread and swap contexts to start executing
  */
-void schedule(int signum){
-
-  // Implement Here
-  //struct threadControlBlock* temp=threadlist;
-  /*while(temp!=NULL){
-    //iterate through list
-    if(temp->status=RUNNABLE){
-      //swapcontext over
-      while(temp->status=RUNNABLE);
-    }
-    temp=temp->next;
+ typdef enum scheduler_status{
+   OFF,
+   ON
+ }sched;
+sched s=OFF;
+void executeToList(struct threadControlBlock** queue){
+  if((*queue)!=NULL){
+    execute->next=(*queue)->next;
+    (*queue)->next=execute;
+    (*queue)=execute;
+    execute=NULL;
   }
-  */
+  else{
+    execute->next=execute;
+    (*queue)=execute;
+    execute=NULL;
+  }
+}
+void schedule(int signum){
+  if(signum==SIGALRM){
+    if(execute!=NULL)
+      if(execute->status==RUNNABLE){
+        executeToList(&r_tail);
+      }
+      else if(execute->status==SLEEP){
+        executeToList(&s_tail);
+      }
+      else{
+        free(execute);
+      }
+    }
+  }
+  else{
+    printf("%d\n",signum);
+    printf("Something has gone horribly wrong????\n");
+  }
 }
 
 /* Create a new TCB for a new thread execution context and add it to the queue
@@ -36,14 +56,14 @@ void my_pthread_create(my_pthread_t *thread, void*(*function)(void*), void *arg)
   // Implement Here
   //
   //init scheduler
-  if(sched=OFF){
-    sched=ON;
+  if(s=OFF){
+    s=ON;
     //insert settimer code with TIME_QUANTUM_MS for time interval
     //set timer interrupt signal to schedule(int signum)
       //this means in the schedule method just check the signum for whatever number is timer interrupt,
       //if an error besides timer interrupt occurs then I will handle it and exit()
-    //initialize head of list
   }
+  //insert rest of create thread code
 }
 
 /* Give up the CPU and allow the next thread to run.
@@ -52,11 +72,13 @@ void my_pthread_yield(){
 
   // Implement Here
   //set status to pause and context swap
+  schedule(SIGALRM);
 }
 
 /* The calling thread will not continue until the thread with tid thread
  * has finished executing.
  */
+ //needs to be changed, he said it was a one line check after identifying node and calling schedular??
 void my_pthread_join(my_pthread_t thread){
   //replace my_pthread_tcb with whatever the head of the linked list is
   // Implement Here //
@@ -65,7 +87,7 @@ void my_pthread_join(my_pthread_t thread){
     return;
   }
   if(temp->tid==thread){
-    while(temp->status==RUNNABLE){}
+    while(temp->status!=FINISHED){}
     my_pthread_tcb=my_pthread_tcb->next;
     return;
   }
@@ -73,7 +95,7 @@ void my_pthread_join(my_pthread_t thread){
     temp=temp->next;
   }
   if(temp->next!=NULL){
-    while(temp->next->status!=RUNNABLE){}
+    while(temp->next->status!=FINISHED){}
     temp->next=temp->next->next;
   }
 }
@@ -95,5 +117,6 @@ my_pthread_t my_pthread_self(){
 void my_pthread_exit(){
 
   // Implement Here //
-
+  //just set status to finished and call schedule(SIGALRM)
+  //the scheduler will take care of it.
 }
